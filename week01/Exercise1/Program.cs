a71dd6ae2f0eeb82e745c61bc98af379520867b0
yourusernameleonardo-wilches <leo.e.wm@gmail.com>
2025-07-03 01:22:31 -0400

    Variable name change

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,11 +5,11 @@ class Program
     static void Main(string[] args)
     {
         Console.Write("What is your first name? ");
-        string first_named = Console.ReadLine();
+        string firstNamed = Console.ReadLine();
 
         Console.Write("What is your last name? ");
-        string last_named = Console.ReadLine();
+        string lastNamed = Console.ReadLine();
 
-        Console.WriteLine($"Your name is {last_named}, {first_named} {last_named}.");
+        Console.WriteLine($"Your name is {lastNamed}, {firstNamed} {lastNamed}.");
     }
 }
