4cc9374651b4ed1fdd309fdea47b0bc6d90caf5a
yourusernameleonardo-wilches <leo.e.wm@gmail.com>
2025-07-05 03:04:49 -0400

    Exercise 4 completed

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,46 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+        int number;
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        do
+        {
+            Console.Write("Enter number: ");
+            string input = Console.ReadLine();
+            number = int.Parse(input);
+
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+        } while (number != 0);
+
+        int sum = 0;
+        foreach (int num in numbers)
+        {
+            sum += num;
+        }
+
+        float average = ((float)sum) / numbers.Count;
+
+        int largestNumber = 0;
+        foreach (int largest in numbers)
+        {
+            if (largestNumber < largest)
+            {
+                largestNumber = largest;
+            }
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {largestNumber}");
     }
 }
