c3aba0958feae90b86c5c2aecc078e80ef229834
yourusernameleonardo-wilches <leo.e.wm@gmail.com>
2025-07-05 19:10:03 -0400

    Name, exercise and little fixes

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
+        // Exercise 4 - Leonardo Wilches
+
         List<int> numbers = new List<int>();
         int number;
 
@@ -21,6 +23,7 @@ class Program
             }
         } while (number != 0);
 
+        //Sum of numbers
         int sum = 0;
         foreach (int num in numbers)
         {
@@ -29,6 +32,7 @@ class Program
 
         float average = ((float)sum) / numbers.Count;
 
+        //Largest number
         int largestNumber = 0;
         foreach (int largest in numbers)
         {
@@ -38,8 +42,27 @@ class Program
             }
         }
 
+        //Smallest number
+        int smallestNumber = largestNumber;
+        foreach (int smallest in numbers)
+        {
+            if (smallestNumber > smallest && smallest > 0)
+            {
+                smallestNumber = smallest;
+            }
+        }
+
         Console.WriteLine($"The sum is: {sum}");
         Console.WriteLine($"The average is: {average}");
         Console.WriteLine($"The largest number is: {largestNumber}");
+        Console.WriteLine($"The smallest positive number is: {smallestNumber}");
+
+        //Sorted list
+        numbers.Sort();
+        Console.WriteLine("The sorted list is:");
+        foreach (int sorted in numbers)
+        {
+            Console.WriteLine(sorted);
+        }
     }
 }
