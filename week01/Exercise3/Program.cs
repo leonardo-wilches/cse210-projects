f83fdc853d4ae7d525039ecd0f371cc8afd89f79
yourusernameleonardo-wilches <leo.e.wm@gmail.com>
2025-07-05 01:54:03 -0400

    Exercise 3 completed.

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,45 @@
 using System;
+using System.Formats.Asn1;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
-    }
+        string answer = null;
+
+        do
+        {
+            Random randomGenerator = new Random();
+            int number = randomGenerator.Next(1, 11);
+
+            int userGuess;
+            int guesses = 1;
+
+            do
+            {
+                Console.Write("What is your guess? ");
+                string guess = Console.ReadLine();
+                userGuess = int.Parse(guess);
+
+                if (userGuess == number)
+                {
+                    Console.WriteLine($"You guessed it in {guesses} tries!");
+                    Console.WriteLine("Do want to play again?");
+                    answer = Console.ReadLine();
+                }
+                else if (userGuess < number)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else
+                {
+                    Console.WriteLine("Lower");
+                }
+
+                guesses++;
+            } while (userGuess != number);
+        } while (answer == "yes");
+
+        Console.WriteLine("Thanks for playing!");
+    }    
 }
