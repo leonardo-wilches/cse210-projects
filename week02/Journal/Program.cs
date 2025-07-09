0891660ee45ea6e9460a5876814086594a30e962
yourusernameleonardo-wilches <leo.e.wm@gmail.com>
2025-07-08 23:59:43 -0400

    Working on the menu

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -1,9 +1,22 @@
 using System;
+using System.Formats.Asn1;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Journal Project.");
+        string userInput;
+
+        do
+        {
+            Console.WriteLine("Please select one of the following choices:");
+            Console.WriteLine("1. Write");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. Load");
+            Console.WriteLine("4. Save");
+            Console.WriteLine("5. Quit");
+            Console.Write("What do you like to do? ");
+            userInput = Console.ReadLine();
+        } while (userInput != "5");
     }
 }
